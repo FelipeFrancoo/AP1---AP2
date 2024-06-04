fd178c594a45df186c03b11f066f2547c6f5d65e
FelipeFrancoo <163058758+FelipeFrancoo@users.noreply.github.com>
2024-06-04 13:41:10 -0300

    Create Lista 3.c

---
+++ Listas AP1/Lista 3.c
@@ -0,0 +1,78 @@
+1) exe:inteiro
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+ repita
+ escreval("Escolha um exercicio")
+ leia(exe)
+ escolha(exe)
+ caso 1
+ escreval("Tarefa 1")
+ caso 2
+ escreval ("Tarefa 2")
+ caso 3
+ escreval ("Tarefa 3")
+ outrocaso
+ escreval("Nao existe")
+ fimescolha
+ ate (exe<0)
+Fimalgoritmo
+2) num:inteiro
+ nume:inteiro
+Inicio
+ // Seção de Comandos, procedimento, funções, operadores, etc...
+ escreval("Escolha um numero")
+ leia(num)
+ nume<-0
+ repita
+ nume<-nume+1
+ escreval(nume)
+ ate(nume>=num)
+Fimalgoritmo
+3) num:inteiro
+ nume:inteiro
+Inicio
+ // Seção de Comandos, procedimento, funções, operadores, etc...
+ escreval("Escolha um numero")
+ leia(num)
+ nume<-num
+ repita
+ nume<-nume-1
+ escreval(nume)
+ ate(nume<=0)
+Fimalgoritmo
+4) num,tabu:inteiro
+Inicio
+ // Seção de Comandos, procedimento, funções, operadores, etc...
+ escreval("Escolha um numero")
+ repita
+ leia(num)
+ escreval(num*1, num*2, num*3, num*4, num*5, num*6, num*7, num*8, num*9)
+ ate(num>10)
+Fimalgoritmo
+5) num,nume:inteiro
+Inicio
+ // Seção de Comandos, procedimento, funções, operadores, etc...
+ escreval("Escolha um numero")
+ leia(num)
+ nume<-num
+ repita
+ nume<-nume*2
+ escreval(nume)
+ ate(nume<num)
+Fimalgoritmo
+6)
+7)
+8) v,p,valor:real
+Inicio
+ // Seção de Comandos, procedimento, funções, operadores, etc...
+ escreval("Como sera pago, a vista ou parcelado?")
+ leia(valor)
+caso 1
+ v<-valor
+ leia(v)
+ escreval(valor)
+caso 2
+ p<-valor*0.10
+ escreval(p)
+Fimalgoritmo
+9)
